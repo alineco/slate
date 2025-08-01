1f61b76eb4005f0b60c109d4ac195813fc5b8cc8
Joe Anderson <joe@mousetrapped.co.uk>
2025-08-01 10:46:37 +0100

    Revert "Merge branch 'fix/remove-node-selection-direction' into aline"
    This reverts commit 0e990f0ffb05a16a506cf00a8e6d971b02eadaf0, reversing
    changes made to 0bac822c229f172d4c151519cb0a4d0833096504.

--- packages/slate/src/interfaces/transforms/general.ts
+++ packages/slate/src/interfaces/transforms/general.ts
@@ -246,10 +246,8 @@ export const GeneralTransforms: GeneralTransforms = {
 
               let preferNext = false
               if (prev && next) {
-                if (Path.isSibling(prev[1], path)) {
-                  preferNext = false
-                } else if (Path.equals(next[1], path)) {
-                  preferNext = true
+                if (Path.equals(next[1], path)) {
+                  preferNext = !Path.hasPrevious(next[1])
                 } else {
                   preferNext =
                     Path.common(prev[1], path).length <
